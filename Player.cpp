fc147e6b04e4327966d4ab87e64a1161d251ea91
LactoseLover <daniel.siciliano98@gmail.com>
2019-12-19 00:27:55 +0100

    proviamo questo nuovo random

--- Player.cpp
+++ Player.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution<int> dist(0,9);
 
 //---------------------------------------------------------------Comuni a player
 Player::Player()
